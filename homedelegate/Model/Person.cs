5eb4db0ddaba2f8806338feb9831d9bedaacbf1e
nihadaslanli <nihadsasl@code.edu.az>
2024-03-28 16:11:17 +0400

    Add project files.

---
+++ homedelegate/Model/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homedelegate.Model
+{
+    internal class Person
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Adress { get; set; }
+        public double Salary { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"Id:{Id} Name:{Name} Surname:{Surname} Adress:{Adress}Salary:{Salary}";
+        }
+    }
+}
---
+++ homedelegate/Program.cs
@@ -0,0 +1,16 @@
+﻿using homedelegate.Model;
+
+List<Person> persons = new List<Person>
+            {
+            new Person { Name="name1",Surname="surname1",Adress="adres1",Salary = 1000},
+            new Person { Name="name2",Surname="surname2",Adress="adres2",Salary = 4000},
+            new Person { Name="name3",Surname="surname3",Adress="adres3",Salary = 500},
+            };
+
+List<Person> changePerson(persons,p=>p.Salary>1000)
+
+
+            foreach (Person person in persons)
+{
+    Console.WriteLine($"Name:{person.Name},Surname:{person.Surname},Adress:{person.Adress}");
+}
