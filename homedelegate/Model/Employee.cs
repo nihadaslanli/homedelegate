44a2ef17c4f5d704664c4c1686b148a52b62870f
nihadaslanli <nihadsasl@code.edu.az>
2024-03-28 17:19:17 +0400

    feat:"add task3"

---
+++ homedelegate/Model/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homedelegate.Model
+{
+    internal class Employee
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string AuthorName { get; set; }
+        public int Salary { get; set; }
+
+        public int Age { get; set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
--- homedelegate/Program.cs
+++ homedelegate/Program.cs
@@ -38,20 +38,36 @@
 //{
 //    new Book {Id=Guid.NewGuid(),Name="Book1",AuthorName="Nizami" },
 //    new Book {Id=Guid.NewGuid(),Name="Book2",AuthorName="fuzuli" },
-//    new Book {Id=Guid.NewGuid(),Name="Book3",AuthorName="Nizami" } 
+//    new Book {Id=Guid.NewGuid(),Name="Book3",AuthorName="Nizami" }
 
 
 //};
-//Console.WriteLine(FindBookOfNizami(b=>b.AuthorName.ToUpper()=="Nizami".ToUpper(),books));
-//int FindBookOfNizami(Predicate<Book>predicate,List<Book>list)
+//Console.WriteLine(FindBookOfNizami(b => b.AuthorName.Equals( "Nizami"), books));
+//int FindBookOfNizami(Predicate<Book> predicate, List<Book> list)
 //{
 //    return list.FindAll(predicate).Count;
 //}
 
 
 
+#endregion
+#region task3
 
+using homedelegate.Model;
 
+List<Employee> emmployees = new()
+{
+ new(){Id=Guid.NewGuid(),Name="name1",Age=16,Salary=1500 },
+ new(){Id=Guid.NewGuid(),Name="name2",Age=21,Salary=2500 },
+ new(){Id=Guid.NewGuid(),Name="name3",Age=19,Salary=3500 },
+ new(){Id=Guid.NewGuid(),Name="name4",Age=59,Salary=3500 },
+};
+var newList = emmployees.FindAll(e=>e.Age > 20 && e.Age < 40);
+   
+foreach (var item in newList)
+{
+    Console.WriteLine(item);
+}
 
 
 #endregion
